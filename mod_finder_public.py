60b2ddb48e380e8d94b9d1d00a7f0a30fbcc7948
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2021-08-13 18:00:28 +0000

    Add files via upload

--- mod_finder_public.py
+++ mod_finder_public.py
@@ -1,14 +1,15 @@
 # mod_finder.py is used to create ω-mod files for individual species
 # a FASTA file supplies the protein sequences and accession numbers to use
 # Copyright © 2021 Ron Beavis
-# Note: this file is for code analysis only. The anonymized
-# database credentials mean that the code will not run
+# Note: this file is for production only and it should not be shared with
+# correct databases CREDS.
 
 import sys
 import re
 import json
 import os
 import time
+import re
 from datetime import datetime
 import mysql.connector
 
@@ -18,13 +19,29 @@ SP = {	'yeast' : 'Saccharomyces cerevisiae',
 	'human': 'Homo sapiens', 
 	'mouse': 'Mus musculus'}
 
-#dict for database credentials
-#in this case, anonymized and non-functional
-CREDS = {	'ip':'i.i.i.i',
-		'user':'uuuu',
-		'pass':'pppp',
-		'db': 'dddd'}
 
+#in this case, anonymized and non-functional
+#CREDS = {	'ip':'i.i.i.i',
+#		'user':'uuuu',
+#		'pass':'pppp',
+#		'db': 'dddd'}
+
+GENES = {	'semi': set(['ins','ins1','ins2']),
+		'gla': set(['f2','pros1','f10','f9','f7','proc','proz','bglap']),
+		'gG' : set(['uba52','rps27a','ubb','ubc']),
+		'cP' : set([	'fcn1','fcn2','fcn3','fga','c1qa','c1qb','c1qc','adipoq','sftpb',
+				'sftpa2','sftpa1','sftpc','colq','marco','gldn','emid1','emid2',
+				'emilin1','emilin2']),
+		'iY' : set(['tg'])
+	}
+
+def getDescription(_acc,_cursor):
+	sql = "select description from enspmapdb.map where label=%(label)s"
+	_cursor.execute(sql,{'label':_acc})
+	vs = _cursor.fetchall()
+	if vs:
+		return vs[0][0]
+	return ""
 
 #retreive modification information for a specified accession and modification type
 def getModified(_acc,_last,_type,_cursor):
@@ -49,6 +66,8 @@ def getModified(_acc,_last,_type,_cursor):
 		sql = "select at,freq from dim_freq where proseqid=%(proid)s";
 	elif _type == 'GGyl':
 		sql = "select at,freq from ubi_freq where proseqid=%(proid)s";
+	elif _type == 'hydroxyl':
+		sql = "select at,freq from oxy_freq where proseqid=%(proid)s";
 	else:
 		print('Error:bad type specification "%s"' % (_type))
 		exit()
@@ -67,6 +86,25 @@ def getModified(_acc,_last,_type,_cursor):
 			res[i] = 0
 	return res
 
+def check_gene(_d):
+	t = re.sub(r'\:p.+',r'',_d.lower())
+	vs = []
+	for x in GENES:
+		if t in GENES[x]:
+			print('\t%s, %s' % (x,_d))
+			vs.append(x)
+	ls = re.findall('^krt\d+$',t)
+	if len(ls):
+		x = 'cR'
+		print('\t%s, %s' % (x,_d))
+		vs.append(x)
+	ls = re.findall('^col\d+a\d$',t)
+	if len(ls):
+		x = 'cP'
+		print('\t%s, %s' % (x,_d))
+		vs.append(x)
+	return vs
+
 #run through the proteins listed in _p and save information to files
 def load_proteins(_p,_f,_m):
 	#connect to GPMDB
@@ -74,7 +112,7 @@ def load_proteins(_p,_f,_m):
 		conn = mysql.connector.connect(host=CREDS['ip'],
 						database=CREDS['db'],
 						user=CREDS['user'],
-						password=CREDS['pass') 
+						password=CREDS['pass']) 
 	except:
 		print('{Error: Could not connect to database')
 		exit()
@@ -88,8 +126,8 @@ def load_proteins(_p,_f,_m):
 		full_species = SP[species]
 	#compose file header information
 	d = datetime.today().strftime('%Y-%m-%d')
-	o = open('%s_mod.xml' % (species),'w')
-	js = open('%s_mod.json' % (species),'w')
+	o = open('mods/%s_mod.xml' % (species),'w')
+	js = open('json/%s_mod.json' % (species),'w')
 	js.write('%s\n' % (json.dumps({'ENSEMBL': '104', 'species': full_species, 'GPMDB' : d, 'min obs' : _m})))
 	o.write('<?xml version="1.0"?>\n')
 	o.write('<bioml label="%s ENSEMBL v.104 potential modification annotation, gpmdb %s">\n' % (full_species,d))
@@ -111,18 +149,26 @@ def load_proteins(_p,_f,_m):
 			print('%i/%i: %i (%.1f%%) delta: %.1f s, current: %.3f m, remaining: %.3f m' 
 				% (c,plen,md,100.0*float(c)/float(plen),dt,tt/60,rt/60))
 			delta = time.time()
-		seq = proteins[l]
+		seq = _p[l]
 		#dict for storing results, by PTM
-		values = {'acetyl':{},'phosphoryl':{},'dimethyl':{},'GGyl':{}}
+		values = {'acetyl':{},'phosphoryl':{},'dimethyl':{},'GGyl':{},'hydroxyl':{}}
 		#load values
 		for v in values:
 			values[v] = getModified(l,len(seq),v,curses)
 		min_obs = _m
 		mod_types = set()
 		#initialize the dict that will be used to generate single line JSON object
-		pmods = {'accession' : l,'acetyl' : [],'phosphoryl' : [], 'dimethyl' : [], 'GGyl' : [], 'seq' : seq}
+		desc = getDescription(l,curses)
+		pmods = {'accession' : l, 'description': desc, 'acetyl' : [],'phosphoryl' : [], 'dimethyl' : [], 'GGyl' : [], 'seq' : seq}
 		#loop through all residues, updating records for each
+		hydroxyl = 0
 		for a in range(1,len(seq)+1):
+			if a in values['hydroxyl']:
+				if seq[a-1] == 'P' or seq[a-1] == 'K':
+					p = values['hydroxyl'][a]
+					if p >= 20:
+						mod_types.add('oP')
+						hydroxyl += 1
 			if a in values['acetyl']:
 				if seq[a-1] == 'K':
 					p = values['acetyl'][a]
@@ -161,21 +207,47 @@ def load_proteins(_p,_f,_m):
 						mod_types.add('pY')
 					if p > 0:
 						pmods['phosphoryl'].append({'r':seq[a-1],'c':a,'n':p})
+		vs = check_gene(desc)
+		for v in vs:
+			mod_types.add(v)
+		if vs:
+			print('\tmods, ',mod_types)
 		#check to see if the XML file will be updated
 		if len(mod_types):
-			mods = list()
+			mods = set()
 			if 'aK' in mod_types:
-				mods.append('42.010565@]K')
+				mods.add('42.010565@]K')
 			if 'pS' in mod_types:
-				mods.append('79.966331@S')
+				mods.add('79.966331@S')
 			if 'pT' in mod_types:
-				mods.append('79.966331@T')
+				mods.add('79.966331@T')
 			if 'pY' in mod_types:
-				mods.append('79.966331@Y')
+				mods.add('79.966331@Y')
 			if 'dR' in mod_types:
-				mods.append('28.031300@R')
-			line = '<protein label="%s" pmods="%s" />' % (l,','.join(mods))
-			o.write('%s\n' % (line))
+				mods.add('28.031300@]R')
+			if 'cR' in mod_types:
+				mods.add('0.984016@]R')
+			if 'oP' in mod_types and hydroxyl > 4:
+				mods.add('15.994915@K')
+				mods.add('15.994915@P')
+				mods.add('-1@B')
+			elif 'cP' in mod_types:
+				mods.add('15.994915@K')
+				mods.add('15.994915@P')
+				mods.add('-1@B')
+			if 'gla' in mod_types:
+				mods.add('43.989829@E')
+			if 'iY' in mod_types:
+				mods.add('125.896648@Y')
+			if 'gG' in mod_types:
+				mods.add('114.042927@]K')
+				mods.add('-1@B')
+			if 'semi' in mod_types:
+				mods.add('-1@X')
+			pl = ','.join(mods)
+			if len(pl) > 0:
+				line = '<protein label="%s" pmods="%s" />' % (l,pl)
+				o.write('%s\n' % (line))
 			md += 1
 		#record the information in pmods as a line of JSON
 		j = json.dumps(pmods)
@@ -204,7 +276,8 @@ def load_proteins(_p,_f,_m):
 
 def main():
 	#open the specified FASTA file
-	f = open(sys.argv[1],'r')
+	base = sys.argv[1]
+	f = open('fasta/%s' % (base),'r')
 	proteins = dict()
 	label = ''
 	seq = ''
@@ -235,8 +308,8 @@ def main():
 
 	#start getting protein modification information
 
-	print('FASTA: %s\nmin obs: %i\n' % (sys.argv[1],mobs))
-	load_proteins(proteins,sys.argv[1],mobs)
+	print('FASTA: %s\nmin obs: %i\n' % (base,mobs))
+	load_proteins(proteins,base,mobs)
 
 if __name__ == "__main__":
     main()
